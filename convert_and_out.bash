913a9b68d287e0ddea7283c0ca135c4772069639
Tran Khanh Duy <40482367+khanhduytran0@users.noreply.github.com>
2020-08-13 07:35:10 +0700

    Update convert_and_out.bash

--- convert_and_out.bash
+++ convert_and_out.bash
@@ -2,7 +2,7 @@
 set -e
 
 # Create dex without Android SDK...
-wget https://maven.google.com/com/android/tools/r8/2.0.99/r8-2.0.99.jar lib/r8.jar
+wget -nv -O out/r8.jar https://maven.google.com/com/android/tools/r8/2.0.99/r8-2.0.99.jar
 rm -rf out && mkdir out
 cp target/lwjgl-glfw-*.jar out/lwjgl-glfw-classes.jar
 java -cp lib/r8.jar com.android.tools.r8.D8 --output out/lwjgl-glfw-converted.jar out/lwjgl-glfw-classes.jar
