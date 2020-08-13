5039e5e229b47b383e27c49030e44c33258bb237
khanhduytran0 <khanhduytran032@gmail.com>
2020-08-13 09:43:24 +0700

    Fix address()???

--- src/main/java/org/lwjgl/glfw/GLFWErrorCallbackI.java
+++ src/main/java/org/lwjgl/glfw/GLFWErrorCallbackI.java
@@ -38,11 +38,6 @@ public interface GLFWErrorCallbackI extends CallbackI.V {
             dcbArgPointer(args)
         );
     }
-	
-	// FIXME attempt fix
-	default long address() {
-		return 1;
-	}
 
     /**
      * Will be called with an error code and a human-readable description when a GLFW error occurs.
