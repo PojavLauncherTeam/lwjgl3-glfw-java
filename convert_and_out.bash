a048828ed69ea96bbf4fa548441ecec0e9437cac
khanhduytran0 <khanhduytran032@gmail.com>
2020-08-13 07:29:43 +0700

    Auto D8 convert lwjgl-glfw out to dex

---
+++ convert_and_out.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# Create dex without Android SDK...
+wget https://maven.google.com/com/android/tools/r8/2.0.99/r8-2.0.99.jar lib/r8.jar
+rm -rf out && mkdir out
+cp target/lwjgl-glfw-*.jar out/lwjgl-glfw-classes.jar
+java -cp lib/r8.jar com.android.tools.r8.D8 --output out/lwjgl-glfw-converted.jar out/lwjgl-glfw-classes.jar
+
