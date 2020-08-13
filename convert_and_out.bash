9c717085f74c09094bbf3fd4f416e1675256ff89
khanhduytran0 <khanhduytran032@gmail.com>
2020-08-13 14:03:21 +0700

    D8: set --min-api to 21

--- convert_and_out.bash
+++ convert_and_out.bash
@@ -5,5 +5,5 @@ set -e
 wget -nv https://maven.google.com/com/android/tools/r8/2.0.99/r8-2.0.99.jar
 rm -rf out && mkdir out
 cp target/lwjgl-glfw-*.jar out/lwjgl-glfw-classes.jar
-java -cp r8-2.0.99.jar com.android.tools.r8.D8 --output out/lwjgl-glfw-converted.jar out/lwjgl-glfw-classes.jar
+java -cp r8-2.0.99.jar com.android.tools.r8.D8 --min-api 21 --output out/lwjgl-glfw-converted.jar out/lwjgl-glfw-classes.jar
 
