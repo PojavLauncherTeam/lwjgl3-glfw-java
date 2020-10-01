93a8ffda8fa41aad549da42f74c99fab5fb14e4f
khanhduytran0 <khanhduytran032@gmail.com>
2020-10-01 13:49:51 +0700

    Use todo branch of lwjglx

--- ci_build.bash
+++ ci_build.bash
@@ -3,9 +3,10 @@ set -e
 
 cd ..
 git clone https://github.com/PojavLauncherTeam/lwjglx
-cd lwjgl3-glfw-java
+cd lwjglx
+git checkout todo
 
+cd ../lwjgl3-glfw-java
 cp -R ../lwjglx/src/main/java/org src/main/java/
-
 mvn -B package --file pom.xml
 
